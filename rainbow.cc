7ce9f64cdf584fda71664e23c670953555478aa4
muvlon <muvlon@hentzes.de>
2017-01-10 08:18:17 +0100

    maybe just blend?

--- rainbow.cc
+++ rainbow.cc
@@ -67,9 +67,9 @@ static void DrawFull(Canvas *canvas){
   for(int x = 0; x < WIDTH*32; x++) {
     for(int y = 0; y < HEIGHT*32; y++) {
       if(inImage(x, y)){
-        canvas->SetPixel(x, y, leds[x][y][0], leds[x][y][1], leds[x][y][2]);
+        canvas->SetPixel(x, y, leds[x][y][0]/2 + 128, leds[x][y][1]/2 + 128, leds[x][y][2]/2 + 128);
       } else {
-        canvas->SetPixel(x, y, ~leds[x][y][0], ~leds[x][y][1], ~leds[x][y][2]);
+        canvas->SetPixel(x, y, leds[x][y][0], leds[x][y][1], leds[x][y][2]);
       }
       leds[x][y][0] *= DECAY;
       leds[x][y][1] *= DECAY;
